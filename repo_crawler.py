02dc11b9f3498ac95513ca12a3244b5e07a96761
Luis Conejo-Alpizar <ldconejo@uw.edu>
2020-05-18 15:54:40 -0700

    Added capability to read JSON config file

--- repo_crawler.py
+++ repo_crawler.py
@@ -2,7 +2,15 @@
 Developed by ldconejo
 '''
 from github import Github
+import json
 
+def read_config_file(filename):
+    '''
+    Loads configuration parameters, including passwords
+    '''
+    with open(filename) as config_file:
+        config_data = json.load(config_file)
+    print(config_data)
 
 def get_all_repos(organization):
     '''
@@ -21,3 +29,6 @@ def create_html(list_of_links):
     creates and opens a webpage with a list of links 
     '''
     pass
+
+if __name__ == '__main__':
+    read_config_file('config.json')
