53e4f626e40aef58b0f00019a3632d504014c0fd
Luis Conejo-Alpizar <ldconejo@uw.edu>
2020-05-18 16:25:29 -0700

    Now enabled to get all repositories from an organization

--- repo_crawler.py
+++ repo_crawler.py
@@ -10,7 +10,33 @@ def read_config_file(filename):
     '''
     with open(filename) as config_file:
         config_data = json.load(config_file)
-    print(config_data)
+    return config_data
+
+def process_config_data(config_data):
+    '''
+    Returns username, password and organization
+    '''
+    access_token= config_data['user_credentials']['access_token']
+    organization = config_data['organization']['name']
+    return access_token, organization
+
+def log_into_github(access_token):
+    '''
+    Uses access token to log into Github
+    '''
+    logged_user = Github(access_token)
+    return logged_user
+
+def log_into_org(logged_user, organization):
+    '''
+    Logs into an organization with the existing user
+    '''
+    org = logged_user.get_organization(organization)
+    org.login
+    for pull in org.get_repos():
+        print(pull.name)
+
+
 
 def get_all_repos(organization):
     '''
@@ -30,5 +56,16 @@ def create_html(list_of_links):
     '''
     pass
 
+def main():
+    '''
+    Main execution code
+    '''
+    configuration_data = read_config_file('config.json')
+    access_token, organization = process_config_data(configuration_data)
+    logged_user = log_into_github(access_token)
+    log_into_org(logged_user, organization)
+
+
 if __name__ == '__main__':
-    read_config_file('config.json')
+    main()
+    
