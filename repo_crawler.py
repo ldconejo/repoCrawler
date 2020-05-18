100904721759b031c5eea07dc2ba05ea46f96c47
Luis Conejo-Alpizar <ldconejo@uw.edu>
2020-05-18 15:45:02 -0700

    Initial commit

---
+++ repo_crawler.py
@@ -0,0 +1,23 @@
+'''
+Developed by ldconejo
+'''
+from github import Github
+
+
+def get_all_repos(organization):
+    '''
+    Gets a list of all repositories under an organization
+    '''
+    pass
+
+def get_open_pull_requests(repo):
+    '''
+    Returns a list of links to open pull requests for a given repository
+    '''
+    pass
+
+def create_html(list_of_links):
+    '''
+    creates and opens a webpage with a list of links 
+    '''
+    pass
