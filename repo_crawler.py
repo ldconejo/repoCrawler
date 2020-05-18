d93a4b527e8259d2e8de2ca77908542add3e85ec
Luis Conejo-Alpizar <ldconejo@uw.edu>
2020-05-18 16:38:58 -0700

    Now it is able to get open pull requests from all repos in an organization

--- repo_crawler.py
+++ repo_crawler.py
@@ -31,24 +31,24 @@ def log_into_org(logged_user, organization):
     '''
     Logs into an organization with the existing user
     '''
-    org = logged_user.get_organization(organization)
-    org.login
-    for pull in org.get_repos():
-        print(pull.name)
+    logged_org = logged_user.get_organization(organization)
+    logged_org.login
+    return logged_org
 
-
-
-def get_all_repos(organization):
+def get_all_repos(logged_org):
     '''
     Gets a list of all repositories under an organization
     '''
-    pass
+    return logged_org.get_repos()
 
 def get_open_pull_requests(repo):
     '''
     Returns a list of links to open pull requests for a given repository
     '''
-    pass
+    open_pulls = repo.get_pulls(state='open', sort='created', base='master')
+    for pull in open_pulls:
+        print(pull.html_url)
+
 
 def create_html(list_of_links):
     '''
@@ -63,7 +63,10 @@ def main():
     configuration_data = read_config_file('config.json')
     access_token, organization = process_config_data(configuration_data)
     logged_user = log_into_github(access_token)
-    log_into_org(logged_user, organization)
+    logged_org = log_into_org(logged_user, organization)
+    org_repos = get_all_repos(logged_org)
+    for repo in org_repos:
+        get_open_pull_requests(repo)
 
 
 if __name__ == '__main__':
